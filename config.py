136c40eddb9166ea4af1b7b297b44cf09fc52031
DanielRSuarezD <43092743+DanielRSuarezD@users.noreply.github.com>
2026-03-13 23:47:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-TOKEN = "8639458986:AAHTNvPggAzrf81iqPd4XZmwSyg_EzXZdA4"
-ADMIN_CHAT_ID = 310049843
+import os
+
+TOKEN = os.getenv("8639458986:AAHTNvPggAzrf81iqPd4XZmwSyg_EzXZdA4")
+ADMIN_CHAT_ID = int(os.getenv("310049843", "0"))
